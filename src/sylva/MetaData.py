1a02a9edf45e0b19938615fc65cd6902065d1797
Robert Gebauer <robert.gebauer@gmx.de>
2023-10-14 20:55:17 +0200

    Added some more meta data.

--- src/sylva/MetaData.py
+++ src/sylva/MetaData.py
@@ -1,7 +1,10 @@
 
 class MetaData(dict):
-    def __init__(self, start: int, end: int, file_hash: str):
+    def __init__(self, start: int, end: int, deviceLocation: str, filename: str, path: str, file_hash: str):
         super().__init__()
         self["start"] = start
         self["end"] = end
+        self["deviceLocation"] = deviceLocation
+        self["fileName"] = filename
+        self["path"] = path
         self["fileHash"] = file_hash
