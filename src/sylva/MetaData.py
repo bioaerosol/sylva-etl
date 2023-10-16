cba8f0e10dece51e25e7db4e0a8d8784345fd8d5
Robert Gebauer <robert.gebauer@gmx.de>
2023-10-16 20:56:13 +0200

    Moved path to separate setter and bugfix typo.

--- src/sylva/MetaData.py
+++ src/sylva/MetaData.py
@@ -1,24 +1,26 @@
 from datetime import datetime
 
 class MetaData(dict):
-    def __init__(self, start: datetime, end: datetime, deviceLocation: str, filename: str, path: str, file_hash: str, device_type: str):
+    def __init__(self, start: datetime, end: datetime, deviceLocation: str, filename: str, file_hash: str, device_type: str):
         super().__init__()
         self["start"] = start
         self["end"] = end
         self["deviceLocation"] = deviceLocation
         self["fileName"] = filename
-        self["path"] = path
         self["fileHash"] = file_hash
         self["deviceType"] = device_type
 
-    def get_key_fields(self):
-        return {
-            "start": self.get("start"),
-            "end": self.get("end"),
-            "deviceLocation": self.get("deviceLocation"),
-            "fileHash": self.get("fileHash"),
-            "deviceTyp": self.get("deviceType")
-        }
+    def set_archive_path(self, path) -> None:
+        self["path"] = path
+
+    def get_key_fields_array(self):
+        return [
+            { "start": self.get("start") },
+            { "end": self.get("end") },
+            { "deviceLocation": self.get("deviceLocation") },
+            { "fileHash": self.get("fileHash") },
+            { "deviceType": self.get("deviceType") }
+        ]
     
     def get_start(self) -> datetime:
         return self.get("start")
