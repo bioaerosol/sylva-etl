01a72a91c9a62775a00c25d1d0b4c4035ca6e139
Robert Gebauer <robert.gebauer@gmx.de>
2024-01-07 08:40:19 +0100

    Added proper EOLs.

--- src/debian/sylva-etl.cron.d
+++ src/debian/sylva-etl.cron.d
@@ -1,4 +1,3 @@
 */7 * * * *  sylva   flock -n /var/lock/sylva-etl -c "sylva-store" 1>> /var/log/sylva-etl/sylva-store.log 2>> /var/log/sylva-etl/sylva-store-error.log
 */13 * * * *  sylva   flock -n /var/lock/sylva-etl -c "sylva-archive" 1>> /var/log/sylva-etl/sylva-archive.log 2>> /var/log/sylva-etl/sylva-archive-error.log
 0 */3 * * *  sylva   flock -n /var/lock/sylva-etl -c "sylva-clean" 1>> /var/log/sylva-etl/sylva-clean.log 2>> /var/log/sylva-etl/sylva-clean-error.log
-  
